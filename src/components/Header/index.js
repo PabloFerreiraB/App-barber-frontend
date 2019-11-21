cc1a89b419e10e4fbb27aa60176817039c4db22d
appaposentaf@outlook.com <appaposentaf@outlook.com>
2019-11-21 18:36:25 -0300

    Configurado o Header

---
+++ src/components/Header/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import logo from '~/assets/logo-header.svg';
+
+import { Container, Content, Profile } from './styles';
+
+export default function Header() {
+  return (
+    <Container>
+      <Content>
+        <nav>
+          <img src={logo} alt="AppBarbearia" />
+          <Link to="/dashboard">DASHBOARD</Link>
+        </nav>
+
+        <aside>
+          <Profile>
+            <div>
+              <strong>Pablo Ferreira</strong>
+              <Link to="/profile">Meu perfil</Link>
+            </div>
+            <img
+              src="https://api.adorable.io/avatars/50/abott@adorable.png"
+              alt="Pablo Ferreira"
+            />
+          </Profile>
+        </aside>
+      </Content>
+    </Container>
+  );
+}
